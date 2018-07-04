86e112411659f7e743ce6c7349ae5befad92f0c7
Justine Bricknell <justinegayton@gmail.com>
2018-07-04 10:17:56 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,9 @@ class String
   end
 
   def count_sentences
+    self.end_with?("!", ".", "?").split
+    binding.pry
+#split the string up by sentences. return 0 if no strings. a sentence must end on punc and start with a cap. 
 
   end
 end
